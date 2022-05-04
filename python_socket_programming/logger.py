5a036e3c961b39fcd5691a1e08d3a9c312ed6820
SmsS4 <smss.lite@gmail.com>
2022-05-04 12:04:42 +0430

    Add logger

---
+++ python_socket_programming/logger.py
@@ -0,0 +1,11 @@
+import logging
+from logging import getLogger
+
+logging.basicConfig(
+    format='%(asctime)s | %(levelname)s | %(name)s::%(module)s::%(lineno)d | %(message)s',
+    level=logging.DEBUG,
+)
+
+
+def get_logger(name: str) -> logging.Logger:
+    return getLogger(name)
